13ef2728c272813359b0df661fe85984b45535d2
Guido Espinoza <guidoespinozac@gmail.com>
2025-01-02 21:05:24 -0300

    Agregar persistencia de la lista de todos en localStorage y cargar desde localStorage al iniciar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
@@ -8,14 +8,21 @@ const App = () => {
   // * Estado para manejar el valor actual del input
   const [todoValue, setTodoValue] = useState("");
 
+  // * Función para persistir la lista de todos en el localStorage
+  const persistData = (newList) => {
+      localStorage.setItem("todos", JSON.stringify({ todos: newList })); // * Persiste la lista en el localStorage
+  }
+
   // * Función para agregar un nuevo todo a la lista
   const handleAddTodo = (newTodo) => {
     setTodos([...todos, newTodo]); // * Agrega el nuevo todo a la lista existente
+    persistData([...todos, newTodo]); // * Persiste la lista en el localStorage
     setTodoValue(""); // * Limpia el valor del input
   };
 
   // * Función para eliminar un todo según su índice
   const handleDeleteTodo = (index) => {
+    persistData(todos.filter((_, todoIndex) => todoIndex !== index)); // * Persiste la lista en el localStorage
     setTodos(todos.filter((_, todoIndex) => todoIndex !== index)); // * Filtra la lista excluyendo el índice dado
   };
 
@@ -25,19 +32,34 @@ const App = () => {
     handleDeleteTodo(index); // * Elimina el todo de la lista para evitar duplicados
   };
 
+  // * Efecto para cargar los todos desde el localStorage
+  useEffect(() => {
+    if (!localStorage) { // * Verifica si el localStorage está disponible
+      return;
+    }
+
+    let localTodos = localStorage.getItem("todos"); // * Obtiene la lista de todos del localStorage
+    if (!localTodos) { // * Verifica si la lista de todos existe
+      return;
+    }
+    localTodos = JSON.parse(localTodos).todos; // * Parsea la lista de todos a JSON
+    setTodos(localTodos); // * Carga la lista de todos en el estado
+
+  }, [])
+
   return (
     <>
       {/* Componente de entrada para agregar nuevos todos */}
-      <TodoInput 
-        todoValue={todoValue} 
-        setTodoValue={setTodoValue} 
-        handleAddTodo={handleAddTodo} 
+      <TodoInput
+        todoValue={todoValue}
+        setTodoValue={setTodoValue}
+        handleAddTodo={handleAddTodo}
       />
       {/* Componente que lista los todos existentes */}
-      <TodoList 
-        todos={todos} 
-        handleEditTodo={handleEditTodo} 
-        handleDeleteTodo={handleDeleteTodo} 
+      <TodoList
+        todos={todos}
+        handleEditTodo={handleEditTodo}
+        handleDeleteTodo={handleDeleteTodo}
       />
     </>
   );
